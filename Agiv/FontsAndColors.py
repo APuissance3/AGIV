a2b764727b1380957686e9a265eb8b6578e6973c
Eddy <119509662+Ap3-ELA@users.noreply.github.com>
2024-11-21 15:46:37 +0100

    Calibration monotache

---
+++ Agiv/FontsAndColors.py
@@ -0,0 +1,14 @@
+# This Python file uses the following encoding: utf-8
+
+from PySide2.QtGui import QColor
+
+INFO_FONT = 12.0
+NORMAL_FONT = 14.0
+BIG_FONT = 18.0
+SMALL_FONT = 10.0
+
+
+q_red_color = QColor('red') 
+q_green_color = QColor('green') 
+q_black_color = QColor('black') 
+q_orange_color = QColor(0xF76E04)
